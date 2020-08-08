dea9a42c2e52d5d5307a2d9905dc8214c017d8b9
Manuel <mrgiraldoo@gmail.com>
2020-08-08 18:13:00 -0500

    Playing with Django secret key

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'tg)9i@g)o4*6_mhc#r-#g5#=$3+doqv^fsflgb+zqn&4q)k3l3'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'tg)9i@g)o4*6_mhc#r-#g5#=$3+doqv^fsflgb+zqn&4q)k3l3')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
